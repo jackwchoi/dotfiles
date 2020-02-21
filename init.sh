8c3b2e99378f52746c9a5610eb1be3f8f237b157
Jack <jackwchoi@pm.me>
2020-02-20 20:56:32 -0500

    update

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 printf '\nManually Copying...\n' >&2
 find backups/Users/* -mindepth 1 -maxdepth 1 |
   while read FILE; do
-   echo cp -rv "$FILE" ~/  # in the future make a hard link?
+   cp -rv "$FILE" ~/  # in the future make a hard link?
   done
 
 # Homebrew
@@ -12,27 +12,24 @@ printf '\nChecking HomeBrew...\n' >&2
 if ! which brew; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-brew bundle install --file='manual-backups/Brewfile-bare'
+brew bundle install --file='manual-backups/Brewfile'
 
-printf '\nChecking VimPlug...\n' >&2
 # Vim Plug
+printf '\nChecking VimPlug...\n' >&2
 if ! [[ -f "$HOME/.vim/autoload/plug.vim" ]]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
-for _ in 1 2; do
-  vim +PlugInstall +qall
-done
-
-printf '\nChecking Vundle...\n' >&2
 # Vundle
+printf '\nChecking Vundle...\n' >&2
 if ! [[ -d "$HOME/.vim/bundle/Vundle.vim" ]]; then
   git clone 'https://github.com/VundleVim/Vundle.vim.git' ~/.vim/bundle/Vundle.vim
 fi
 
 for _ in 1 2; do
-  vim +PluginInstall +qall
+  vim +PlugInstall   +qall  # vimplug
+  vim +PluginInstall +qall  # vundle
 done
 
 printf '\nChecking Cargo...\n' >&2
