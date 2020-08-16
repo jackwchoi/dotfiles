5e485d453ccf7f400918fa5ebad87ba715e1ad3b
Jack <jackwchoi@pm.me>
2020-08-16 02:03:34 -0400

    update

--- backups/Users/jackwchoi/run.sh
+++ backups/Users/jackwchoi/run.sh
@@ -52,7 +52,7 @@ function clean_quick {
   {
     find ~/ -maxdepth 1
     find "$WORKSPACE"
-    find ~/{Desktop,Documents,Downloads}/
+    find ~/{Desktop,Downloads}/
     find ~/tresor-*
   } |
     rg "/(\.$REPL|\.$MISC|__pycache__)$" |
